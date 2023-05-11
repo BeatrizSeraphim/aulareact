a7df59613bd650640168f1a2cdc899d5dbe7e0fd
Beatriz Seraphim <beatrizoseraphim@gmail.com>
2023-05-11 14:48:25 -0300

    React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+
+        <>
+            <h1 className='titulo'>O mundo dos doces</h1>
+            <img className='img' src="https://i.gifer.com/origin/00/00a1151474fb4204febea5920992bf7c.gif" alt=''></img>
+        </>
+    )
+}
+
+export default Home;
